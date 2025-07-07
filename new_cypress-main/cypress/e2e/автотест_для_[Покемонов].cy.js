b63382a59ec501a9d71c30dc285e175fe7137fb0
artemev-artem <grot10875@gmail.com>
2025-07-07 23:38:40 +0300

    Update автотест_для_[Покемонов].cy.js
    скрыл логин

--- new_cypress-main/cypress/e2e/автотест_для_[Покемонов].cy.js
+++ new_cypress-main/cypress/e2e/автотест_для_[Покемонов].cy.js
@@ -4,7 +4,7 @@ it('1. проверка покупки аватара', function () {
         cy.visit('https://pokemonbattle.ru'); // открой сайт 
     cy.get('.style_1_popup_white_in')
     cy.get('.style_1_popup_white_title').contains('Битва покемонов');    
-    cy.get('#k_email').type('grot10875@yandex.ru');
+    cy.get('#k_email').type('USER_LOGIN');
     cy.get('#k_password').type('USER_PASSWORD');
     cy.get('.MuiButton-root').should('be.visible'); 
     cy.get('.MuiButton-root').should('have.css', 'color', 'rgb(255, 255, 255)'); 
